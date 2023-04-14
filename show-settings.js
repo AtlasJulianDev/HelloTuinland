773251fcbed474e0dd3896331dd3ee4862b06185
AtlasJulianDev <130748452+AtlasJulianDev@users.noreply.github.com>
2023-04-14 11:50:20 +0200

    Delete show-settings.js

--- show-settings.js
+++
@@ -1,9 +0,0 @@
-window.TrelloPowerUp.initialize({
-    'show-settings': function(t, options){
-        return t.popup({
-            title: "Custom Fields Settings",
-            url: "settings.html",
-            height: 184
-        })
-    }
-})
