aff5b89e982b719d45c5e000c47999ece79b1c02
AtlasJulianDev <130748452+AtlasJulianDev@users.noreply.github.com>
2023-04-14 12:07:38 +0200

    Add files via upload

--- card-badges.js
+++ card-badges.js
@@ -1,10 +1,7 @@
 window.TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
       let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
-      return t
-        .card("name")
-        .get("name")
-        .then(function (cardName) {
+      return t.card("name").get("name").then(function (cardName) {
           console.log("We just loaded the card name for fun: " + cardName);
           return [
             {
