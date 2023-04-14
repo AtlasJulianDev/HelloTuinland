23e784e3810d2e916646c59105f686b6654397ec
AtlasJulianDev <130748452+AtlasJulianDev@users.noreply.github.com>
2023-04-14 11:48:41 +0200

    :sob:

--- show-settings.js
+++ show-settings.js
@@ -2,7 +2,7 @@ window.TrelloPowerUp.initialize({
     'show-settings': function(t, options){
         return t.popup({
             title: "Custom Fields Settings",
-            url: "./settings.html",
+            url: "settings.html",
             height: 184
         })
     }
